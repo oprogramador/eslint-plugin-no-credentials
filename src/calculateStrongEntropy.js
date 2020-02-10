32d9cd25f30c87d968d7fa40319a3800318a1975
oprogramador <uomodislesia@gmail.com>
2020-02-10 19:22:11 +0100

    gzipSync

--- src/calculateStrongEntropy.js
+++ src/calculateStrongEntropy.js
@@ -1,16 +1,9 @@
 import calculateEntropy from 'eslint-plugin-no-credentials/calculateEntropy';
 import zlib from 'zlib';
 
-const gzip = string => new Promise((resolve, reject) => zlib.gzip(string, (error, result) => {
-  if (error) {
-    reject(error);
-  }
-  resolve(result);
-}));
-
-async function calculateStrongEntropy(string) {
+function calculateStrongEntropy(string) {
   const entropy = calculateEntropy(string);
-  const zipped = await gzip(string);
+  const zipped = zlib.gzipSync(string);
 
   return Math.sqrt((entropy + 1) * (zipped.length - 20));
 }
--- src/tests/calculateStrongEntropy.js
+++ src/tests/calculateStrongEntropy.js
@@ -2,138 +2,138 @@ import calculateStrongEntropy from 'eslint-plugin-no-credentials/calculateStrong
 import expect from 'eslint-plugin-no-credentials/tests/expect';
 
 describe('calculateStrongEntropy', () => {
-  it('calculates strong entropy for an empty string', async () => {
+  it('calculates strong entropy for an empty string', () => {
     const string = '';
 
-    const result = await calculateStrongEntropy(string);
+    const result = calculateStrongEntropy(string);
 
     expect(result).to.be.closeTo(0, 0.00001);
   });
 
-  it('calculates strong entropy for a', async () => {
+  it('calculates strong entropy for a', () => {
     const string = 'a';
 
-    const result = await calculateStrongEntropy(string);
+    const result = calculateStrongEntropy(string);
 
     expect(result).to.be.closeTo(1, 0.00001);
   });
 
-  it('calculates strong entropy for aaaaa', async () => {
+  it('calculates strong entropy for aaaaa', () => {
     const string = 'aaaaa';
 
-    const result = await calculateStrongEntropy(string);
+    const result = calculateStrongEntropy(string);
 
     expect(result).to.be.closeTo(1.73205, 0.00001);
   });
 
-  it('calculates strong entropy for abcde', async () => {
+  it('calculates strong entropy for abcde', () => {
     const string = 'abcde';
 
-    const result = await calculateStrongEntropy(string);
+    const result = calculateStrongEntropy(string);
 
     expect(result).to.be.closeTo(4.07549, 0.00001);
   });
 
-  it('calculates strong entropy for foo', async () => {
+  it('calculates strong entropy for foo', () => {
     const string = 'foo';
 
-    const result = await calculateStrongEntropy(string);
+    const result = calculateStrongEntropy(string);
 
     expect(result).to.be.closeTo(2.39893, 0.00001);
   });
 
-  it('calculates strong entropy for foofoofoo', async () => {
+  it('calculates strong entropy for foofoofoo', () => {
     const string = 'foofoofoo';
 
-    const result = await calculateStrongEntropy(string);
+    const result = calculateStrongEntropy(string);
 
     expect(result).to.be.closeTo(3.09701, 0.00001);
   });
 
-  it('calculates strong entropy for foo repeated 10 times', async () => {
+  it('calculates strong entropy for foo repeated 10 times', () => {
     const string = 'foo'.repeat(10);
 
-    const result = await calculateStrongEntropy(string);
+    const result = calculateStrongEntropy(string);
 
     expect(result).to.be.closeTo(3.09701, 0.00001);
   });
 
-  it('calculates strong entropy for foo repeated 100 times', async () => {
+  it('calculates strong entropy for foo repeated 100 times', () => {
     const string = 'foo'.repeat(100);
 
-    const result = await calculateStrongEntropy(string);
+    const result = calculateStrongEntropy(string);
 
     expect(result).to.be.closeTo(3.66443, 0.00001);
   });
 
-  it('calculates strong entropy for foo1foo2foo', async () => {
+  it('calculates strong entropy for foo1foo2foo', () => {
     const string = 'foo1foo2foo';
 
-    const result = await calculateStrongEntropy(string);
+    const result = calculateStrongEntropy(string);
 
     expect(result).to.be.closeTo(4.85332, 0.00001);
   });
 
-  it('calculates strong entropy for foobarbaz', async () => {
+  it('calculates strong entropy for foobarbaz', () => {
     const string = 'foobarbaz';
 
-    const result = await calculateStrongEntropy(string);
+    const result = calculateStrongEntropy(string);
 
     expect(result).to.be.closeTo(5.61509, 0.00001);
   });
 
-  it('calculates strong entropy for a random 9-characters string', async () => {
+  it('calculates strong entropy for a random 9-characters string', () => {
     const string = 'wYPT0KmIp';
 
-    const result = await calculateStrongEntropy(string);
+    const result = calculateStrongEntropy(string);
 
     expect(result).to.be.closeTo(6.12611, 0.00001);
   });
 
-  it('calculates strong entropy for a random 12-characters string', async () => {
+  it('calculates strong entropy for a random 12-characters string', () => {
     const string = 'tBfsfGjuw7Nc';
 
-    const result = await calculateStrongEntropy(string);
+    const result = calculateStrongEntropy(string);
 
     expect(result).to.be.closeTo(7.28145, 0.00001);
   });
 
-  it('calculates strong entropy for a random 12-characters 01 sequence', async () => {
+  it('calculates strong entropy for a random 12-characters 01 sequence', () => {
     const string = '101001010001';
 
-    const result = await calculateStrongEntropy(string);
+    const result = calculateStrongEntropy(string);
 
     expect(result).to.be.closeTo(4.22123, 0.00001);
   });
 
-  it('calculates strong entropy for a random 32-characters 01 sequence', async () => {
+  it('calculates strong entropy for a random 32-characters 01 sequence', () => {
     const string = '10001011101100010100100011000111';
 
-    const result = await calculateStrongEntropy(string);
+    const result = calculateStrongEntropy(string);
 
     expect(result).to.be.closeTo(5.82684, 0.00001);
   });
 
-  it('calculates strong entropy for a random 12-characters digits sequence', async () => {
+  it('calculates strong entropy for a random 12-characters digits sequence', () => {
     const string = '676158717724';
 
-    const result = await calculateStrongEntropy(string);
+    const result = calculateStrongEntropy(string);
 
     expect(result).to.be.closeTo(6.55892, 0.00001);
   });
 
-  it('calculates strong entropy for a random 32-characters digits sequence', async () => {
+  it('calculates strong entropy for a random 32-characters digits sequence', () => {
     const string = '45220077709114486712228402662775';
 
-    const result = await calculateStrongEntropy(string);
+    const result = calculateStrongEntropy(string);
 
     expect(result).to.be.closeTo(10.55522, 0.00001);
   });
 
-  it('calculates strong entropy for aą#$@;,vŽžő', async () => {
+  it('calculates strong entropy for aą#$@;,vŽžő', () => {
     const string = 'aą#$@;,vŽžő';
 
-    const result = await calculateStrongEntropy(string);
+    const result = calculateStrongEntropy(string);
 
     expect(result).to.be.closeTo(8.44694, 0.00001);
   });
